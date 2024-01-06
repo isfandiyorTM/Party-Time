89cd3026f861c6a270841715fa52dd466f932491
Isfandiyor Madminov <isfandiyormadaminov12@gmail.com>
2024-01-06 08:58:25 +0500

    add animation

--- js/app.js
+++ js/app.js
@@ -56,11 +56,81 @@ menu_btn.addEventListener("click", () => {
 
 // Window eventlistener
 const nav = document.querySelector("nav");
+const logo = document.querySelector(".logo");
+const nav_links = document.querySelectorAll("header nav .navigation a");
+const header_popular = document.querySelector("#popular-parties .heading");
+const header_parties = document.querySelector("#package-offers .heading");
+const parties_cards = document.querySelectorAll(
+    "#package-offers .cards-container .card"
+);
+const gallery_heading = document.querySelector("#gallery .heading");
+const gallery_items = document.querySelectorAll(
+    "#gallery .image-container .image"
+);
+
 window.addEventListener("scroll", () => {
+    const triggerBottom = (window.innerHeight / 5) * 4.5;
+
+    let header_popular_top = header_popular.getBoundingClientRect().top;
+    if (header_popular_top < triggerBottom) {
+        header_popular.classList.add("show");
+    } else {
+        header_popular.classList.remove("show");
+    }
+
+    let popular_parties_container_top =
+        popular_parties_container.getBoundingClientRect().top;
+    if (popular_parties_container_top < triggerBottom) {
+        popular_parties_container.classList.add("show");
+    } else {
+        popular_parties_container.classList.remove("show");
+    }
+
+    let header_parties_top = header_parties.getBoundingClientRect().top;
+    if (header_parties_top < triggerBottom) {
+        header_parties.classList.add("show");
+    } else {
+        header_parties.classList.remove("show");
+    }
+
+    parties_cards.forEach((parties_container) => {
+        let parties_container_top = parties_container.getBoundingClientRect().top;
+        if (parties_container_top < triggerBottom) {
+            parties_container.classList.add("show");
+        } else {
+            parties_container.classList.remove("show");
+        }
+    });
+
+    let gallery_heading_top = gallery_heading.getBoundingClientRect().top;
+    if (gallery_heading_top < triggerBottom) {
+        gallery_heading.classList.add("show");
+    } else {
+        gallery_heading.classList.remove("show");
+    }
+
+    gallery_items.forEach((item) => {
+        let item_top = item.getBoundingClientRect().top;
+        if (item_top < triggerBottom) {
+            item.classList.add("show");
+        } else {
+            item.classList.remove("show");
+        }
+    });
+
+    // Nav animation
     menu.classList.remove("active");
-    if (window.scrollY > 700) {
+    if (window.scrollY > 120) {
         nav.classList.add("active");
+        logo.classList.add("active");
+        nav_links.forEach((nav_link) => {
+            nav_link.classList.add("active");
+        });
     } else {
         nav.classList.remove("active");
+        logo.classList.remove("active");
+        nav_links.forEach((nav_link) => {
+            nav_link.classList.remove("active");
+        });
     }
 });
